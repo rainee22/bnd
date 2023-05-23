25432c36141ca4d1e2383b429b8147257e8f0b07
centzkie <centzkie14@gmail.com>
2023-05-23 13:00:16 +0800

    Update

--- resources/views/livewire/main/components/projects.blade.php
+++ resources/views/livewire/main/components/projects.blade.php
@@ -6,7 +6,7 @@
                 Some of our projects are included here
             </h5>
         </div>
-        <div id="carouselExampleIndicators" class="carousel slide"  data-ride="carousel">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="5000">
             <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -14,14 +14,20 @@
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <iframe class="d-block w-100" src="https://drive.google.com/file/d/1Qz-sHdTM13jTuPBeKCiaP9Ay4vjMakjN/preview" width="440" height="480" allow="autoplay" alt="Third slide"></iframe>                
-                </div>
-                 <div class="carousel-item">
-                    <iframe class="d-block w-100" src="https://drive.google.com/file/d/1KxGmbgo9DkpnxkX8dWdsGRqKth_nMliS/preview" width="440" height="480" allow="autoplay" alt="Fourth slide"></iframe>                
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="https://drive.google.com/file/d/1Qz-sHdTM13jTuPBeKCiaP9Ay4vjMakjN/preview" allow="autoplay"></iframe>
+                    </div>
                 </div>
                 <div class="carousel-item">
-                    <iframe class="d-block w-100" src="https://drive.google.com/file/d/1MfrrADs-ar4BXwx9JFjw4FdynXlBpVIT/preview" width="440" height="480" allow="autoplay" alt="Fifthh slide"></iframe>                
-                </div>         
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="https://drive.google.com/file/d/1KxGmbgo9DkpnxkX8dWdsGRqKth_nMliS/preview" allow="autoplay"></iframe>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="https://drive.google.com/file/d/1MfrrADs-ar4BXwx9JFjw4FdynXlBpVIT/preview" allow="autoplay"></iframe>
+                    </div>
+                </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -31,7 +37,11 @@
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="sr-only">Next</span>
             </a>
-        </div><br><br>
+        </div>
+        
+        
+        
+        <br><br>
         <h5 class="fw-bold mb-4">These are some of the website links available</h5>
         <ul class="fw-bold">
         <li> <a href="#">Link Sample 1</a> </li>
