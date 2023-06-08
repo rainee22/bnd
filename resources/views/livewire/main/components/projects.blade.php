7f8592e840d75545bb924236a6b5494fa509e8bf
Chanellaaa <chanellaaquino@gmail.com>
2023-06-08 16:47:47 +0800

    my changes

--- resources/views/livewire/main/components/projects.blade.php
+++ resources/views/livewire/main/components/projects.blade.php
@@ -35,8 +35,8 @@
                </div>
 
                 <div class="col">
-                  <button type="button" class="modalButton" data-toggle="modal" data-src="https://drive.google.com/file/d/1FNk9kfF4B96Iw9wHeBgPXKF7zOoqA5gm/preview"  data-width="600" data-height="450" data-target="#myModal" data-video-fullscreen="">
-                  <img src="{{ asset('images/img5.png') }}" style="background-color: #ffffff; padding: 15px; width: 100%; box-shadow: 0 0 15px rgba(0,0,0,0.3); cursor: pointer;" title="Barangay Information System"></button>
+                  <button type="button" class="modalButton" data-toggle="modal" data-src="https://drive.google.com/file/d/1K8N921kZTW7TbPLPNzs63lXLya0CedVx/preview"  data-width="600" data-height="450" data-target="#myModal" data-video-fullscreen="">
+                  <img src="{{ asset('images/img6.png') }}" style="background-color: #ffffff; padding: 15px; width: 100%; box-shadow: 0 0 15px rgba(0,0,0,0.3); cursor: pointer;" title="Legislative Tracking System"></button>
                </div>
             </div>
             </div>
